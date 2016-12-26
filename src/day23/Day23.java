29b2aad3af607bd2bee2af2c71aa59c99312c72b
Johan 'Horv' Andersson <drhorv@gmail.com>
2016-12-27 00:17:08 +0100

    Added refactored version of day23

--- src/day23/Day23.java
+++ src/day23/Day23.java
@@ -2,9 +2,11 @@ package day23;
 
 import utils.FileReader;
 import utils.Solver;
+import utils.assembunny.AssemBunny;
+import utils.assembunny.Instruction;
+import utils.assembunny.Parser;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class Day23 implements Solver {
@@ -15,7 +17,7 @@ public class Day23 implements Solver {
 
         AssemBunny bunny = new AssemBunny();
 
-        ArrayList<Instruction> instructions = parseInstructions(inputArr);
+        ArrayList<Instruction> instructions = Parser.parseInstructions(inputArr);
 
         //instructions.stream().forEach(System.out::println);
         //System.out.println();
@@ -51,8 +53,6 @@ public class Day23 implements Solver {
 
         }
 
-
-
         return "" + bunny.getRegistry("a");
 
 
@@ -60,7 +60,7 @@ public class Day23 implements Solver {
     }
 
     private void doToggle(int index, String arg, AssemBunny bunny, ArrayList<Instruction> instructions) {
-        int deltaIndex = getValue(arg, bunny);
+        int deltaIndex = Parser.getValue(arg, bunny);
         int value = index + deltaIndex;
         if(value >= instructions.size() || value < 0) {
             return;
@@ -70,53 +70,12 @@ public class Day23 implements Solver {
     }
 
     private int doJump(String from, String dIndex, AssemBunny bunny) {
-        int value = getValue(from, bunny);
+        int value = Parser.getValue(from, bunny);
         if(value != 0) {
-            return getValue(dIndex, bunny);
+            return Parser.getValue(dIndex, bunny);
         } else {
             return 1;
         }
     }
 
-    private int getValue(String from, AssemBunny bunny) {
-        int value;
-        try {
-            value = Integer.parseInt(from);
-        } catch (NumberFormatException e) {
-            value = bunny.getRegistry(from);
-        }
-        return value;
-    }
-
-    private ArrayList<Instruction> parseInstructions(String[] inputArr) {
-        ArrayList<Instruction> instructions = new ArrayList<>();
-        for(String line : inputArr) {
-            instructions.add(parseInstruction(line));
-        }
-        return instructions;
-    }
-
-    private Instruction parseInstruction(String line) {
-        String[] parts = line.split(" ");
-        String[] args = Arrays.copyOfRange(parts, 1, parts.length);
-        Instruction.Type t = parseType(parts[0]);
-        return new Instruction(t, args);
-    }
-
-    private Instruction.Type parseType(String part) {
-        switch (part) {
-            case "cpy":
-                return Instruction.Type.COPY;
-            case "inc":
-                return Instruction.Type.INC;
-            case "dec":
-                return Instruction.Type.DEC;
-            case "jnz":
-                return Instruction.Type.JUMP;
-            case "tgl":
-                return Instruction.Type.TOGGLE
-                        ;
-        }
-        return Instruction.Type.NOP;
-    }
 }
