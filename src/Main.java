281ea4112ba7b8a9046e51dc990c326325ff1756
Johan 'Horv' Andersson <drhorv@gmail.com>
2016-12-27 01:58:37 +0100

    Final main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
+import day09.Day9;
 import day10.Day10;
 import day11.Day11;
 import day12.Day12;
 import day13.Day13;
+import day14.Day14;
+import day15.Day15;
+import day16.Day16;
+import day17.Day17;
+import day18.Day18;
+import day19.Day19;
+import day20.Day20;
+import day21.Day21;
+import day22.Day22;
+import day23.Day23;
+import day24.Day24;
+import day25.Day25;
 
 public class Main {
 
@@ -19,7 +32,19 @@ public class Main {
         //System.out.println(new Day10().solve());
         //System.out.println(new Day11().solve());
         //System.out.println(new Day12().solve());
-        System.out.println(new Day13().solve());
+        //System.out.println(new Day13().solve());
+        //System.out.println(new Day14().solve());
+        //System.out.println(new Day15().solve());
+        //System.out.println(new Day16().solve());
+        //System.out.println(new Day17().solve());
+        //System.out.println(new Day18().solve());
+        //System.out.println(new Day19().solve());
+        //System.out.println(new Day20().solve());
+        //System.out.println(new Day21().solve());
+        System.out.println(new Day22().solve());
+        //System.out.println(new Day23().solve());
+        //System.out.println(new Day24().solve());
+        //System.out.println(new Day25().solve());
 
         long now = System.currentTimeMillis();
 
